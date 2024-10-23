39c1bba93720314808641c4f4110bd08b6c8f89f
eze <ezequiel@rodrigoinfante.com.ar>
2024-10-22 22:00:28 -0300

    primera revicion de links

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -7,14 +7,14 @@ function Nadvar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary"  bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand  href="#home">Chill Hotel</Navbar.Brand>
+        <Navbar.Brand  href="#index">Chill Hotel</Navbar.Brand>
         <Navbar.Toggle aria-controls="boton-hamburguesa" />
         <Navbar.Collapse id="boton-hamburguesa">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Hoteles</Nav.Link>
-            <Nav.Link href="#link">Ciudades</Nav.Link>
-              <Nav.Link href="#action/3.1">Sobre nosotros</Nav.Link>
-              <Nav.Link href="#action/3.2">
+            <Nav.Link href="#index">Hoteles</Nav.Link>
+            <Nav.Link href="#ciudades">Ciudades</Nav.Link>
+              <Nav.Link href="#nosotros">Sobre nosotros</Nav.Link>
+              <Nav.Link href="#registro">
                 Registrarce
               </Nav.Link>     
               
