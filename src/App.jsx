4907a30665bb9eb1f461c6baf76e7865d9eb1465
eze <ezequiel@rodrigoinfante.com.ar>
2024-10-22 21:51:12 -0300

    primer nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Nadvar from './nav'
 function App() {
 
   return (
     <>
+    <nav><Nadvar></Nadvar></nav>
      
     </>
   )
